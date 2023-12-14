80a44c3a3b5e547835e940a9c3b9b5408e37e136
Y4N1SSSSS <153291185+Y4N1SSSSS@users.noreply.github.com>
2023-12-14 14:27:03 +0100

    Création de toute l'arborescence de base
    Création de toute l'arborescence de base  en important les librairies JavaScipt élémentaire et en organisant correctement tout les fichiers

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include "php/header.php" ;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script src="js/jquery-3.6.4.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Accueil</title>
+</head>
--- php/header.php
+++ php/header.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css">
+  <script src="../js/jquery-3.6.4.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/style.css">
   <title>Accueil</title>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="index.html">
-    <img src="votre-logo.png" alt="Restaure Ton Patrimoine" height="30">
+<nav class="navbar navbar-container navbar-expand-lg navbar-light couleurnav">
+  <a class="navbar-brand" href="../index.html">
+    
   </a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,18 +23,17 @@
   <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">Accueil</a>
+        <a class="nav-link" href="../index.html">Accueil</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="inscription.html">inscription</a>
+        <a class="nav-link" href="../pages/inscription.html">Inscription</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="donation.html">Donation</a>
+        <a class="nav-link" href="../pages/donation.html">Donation</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="planning.html">Planning</a>
+        <a class="nav-link" href="../pages/planning.html">Planning</a>
       </li>
     </ul>
   </div>
 </nav>
-
