f2d4708189fc1fea7d4bc30e18817a0eb729f1fc
RafaelReyes0707 <reyes.rafael0707@gmail.com>
2023-12-18 17:46:53 +0100

    Ajout d'un footer
    Avec des images et les liens de contacts par encore modifiés, dans un fichier php pour pouvoir être inclus partout.

---
+++ php/footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script src="../js/jquery-3.6.4.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<footer class="footer text-light">
+    <div class="container py-4">
+        <div class="row">
+            <!-- Colonne Contacts -->
+            <div class="col-md-4">
+                <h4 class="titresFooter">Contact</h4>
+                <p class="textFooter">Adresse: 123 Rue de l'Association</p>
+                <p class="textFooter">Email: info@association.org</p>
+                <p class="textFooter">Téléphone: +1 234 567 890</p>
+            </div>
+
+            <!-- Colonne Réseaux sociaux -->
+            <div class="col-md-4">
+                <h4 class="titresFooter">Réseaux sociaux</h4>
+                    <a href="https://twitter.com/" target="_blank"><img src="../images/X.png" class='imagesfooter'></a>
+                    <a href="https://www.facebook.com/" target="_blank"><img src="../images/F.png" class='imagesfooter'></a>
+                    <a href="https://www.instagram.com/" target="_blank"><img src="../images/I.png" class='imagesfooter'></a>
+            </div>
+
+            <!-- Colonne Logo -->
+            <div class="col-md-4">
+                <h5>Logo</h5>
+                <img src="path/to/your/logo.png" alt="Logo de l'association" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
