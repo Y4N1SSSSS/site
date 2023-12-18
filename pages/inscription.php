e435b87bc8c61506d1a4f81e1ffe75fe08c7b138
RafaelReyes0707 <reyes.rafael0707@gmail.com>
2023-12-18 11:11:38 +0100

    Création de la page inscription.php

---
+++ pages/inscription.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href='../css/styledonations.css' rel='stylesheet'>
+    <title>Inscrivez-Vous!</title>
+</head>
+    <!-- Ajout du header -->
+    <?php
+        include "../php/header.php" ;
+    ?>
+<body>
+    
+</body>
+</html>
