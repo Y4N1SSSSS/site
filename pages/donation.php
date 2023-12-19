88bf50658bd875b5244432d441d8e2d58204a579
Y4N1SSSSS <153291185+Y4N1SSSSS@users.noreply.github.com>
2023-12-19 16:11:26 +0100

    Php pour la page donation.php
    Je viens de créer la page donation.php avec la barre qui s'adapte maintenant par rapport à la quantité de donation en argent (la barre se rempli jusqu'à 10000€ max)

--- pages/donation.php
+++ pages/donation.php
@@ -1,67 +1,125 @@
+<?php
+require_once '../php/Config.php';
+
+try {
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+} catch (PDOException $e) {
+echo $e->getMessage();
+}
+
+$requete = 'SELECT SUM(Valeur) AS total FROM donation';
+$resultats = $pdo->query($requete);
+$totalDons = $resultats->fetch(PDO::FETCH_ASSOC)['total'];
+
+echo "<script>
+var totalDons = $totalDons;
+</script>";
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <link href='../css/stylepages.css' rel='stylesheet'>
-    <title>Donations</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+<link href='../css/stylepages.css' rel='stylesheet'>
+<title>Donations</title>
 </head>
-    <!-- Ajout du header -->
-    <?php
-        include "../php/header.php" ;
-    ?>
+<!-- Ajout du header -->
+<?php
+    include "../php/header.php" ;
+?>
 <body>
-    <!-- SECTION DES DONS EN EUROS -->
-    <h1 class="titres">Don Monétaire</h1>
-    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
-    <!-- Section en flexbox pour l'image et le texte -->
-    <div class="container flexd mt-5 mb-3">
-        <img class="imagedon" src="../images/donsEUR.png"> 
-        <span class="spanD"><span class="titredon">Soutenez Notre Patrimoine National</span><br><br>
-            Cher(e) ami(e) de la culture,
-            Votre don contribue à la préservation de notre riche patrimoine français. Aidez-nous à protéger nos trésors historiques et culturels. Chaque euro compte.</span>
+<!-- SECTION DES DONS EN EUROS -->
+<h1 class="titres">Don Monétaire</h1>
+<div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+<!-- Section en flexbox pour l'image et le texte -->
+<div class="container flexd mt-5 mb-3">
+    <img class="imagedon" src="../images/donsEUR.png"> 
+    <span class="spanD"><span class="titredon">Soutenez Notre Patrimoine National</span><br><br>
+        Cher(e) ami(e) de la culture,
+        Votre don contribue à la préservation de notre riche patrimoine français. Aidez-nous à protéger nos trésors historiques et culturels. Chaque euro compte.</span>
+</div>
+<!-- Section en flexbox pour la barre de progression et les boutons -->
+<div class="flexEUR">
+    <div class="txtclasse">
+    <span class="progress-text">Progression  de  l'objectif  de  donation : </span>
+    <!-- Barre de progression qui fonctionne avec le Javascript -->
+    <div class="progress-container mt-3">
+    <div class="progress-bar" id="myBar"></div>
     </div>
-    <!-- Section en flexbox pour la barre de progression et les boutons -->
-    <div class="flexEUR">
-        <div class="txtclasse">
-        <span class="progress-text">Progression  de  l'objectif  de  donation : </span>
-        <!-- Barre de progression qui fonctionne avec le Javascript -->
-        <div class="progress-container mt-3">
-            <div class="progress-bar" id="myBar"></div>
-        </div>
-        </div>
-        <!-- BOUTON POUR LES DONS ET INFORMATIONS -->
-        <a class="bouton NS mt-3" onclick="move()">Faire un don</a><a class="bouton NS mt-3">En savoir plus</a>
     </div>
-    <div class="back">
-        <!-- DECTION DES DONS DE MATÉRIEL -->
-    <h1 class="titres">Don Matériel</h1>
-    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
-    <!-- Section en flexbox pour l'image et le texte -->
-    <div class="mt-5 container flex1">
-        <span class="spanobj"><span class="titredon">Aidez-nous a Préserver Notre Culture</span><br><br>
-        Chaque geste contribue à tisser la toile de notre passé, créant un lien tangible entre hier, aujourd'hui et demain. Rejoins-nous dans cette aventure de préservation, car la restauration de notre patrimoine commence avec toi.<br></span>
-        <img class="imagedonOBJ" src="../images/donsOBJ.png"> 
-        </div>
-        <!-- BOUTON POUR LES DONS ET INFORMATIONS -->
-        <div class="mt-5 d-flex justify-content-center mb-5">
-        <a class="bouton NS">Faire un don</a> <a class="bouton NS">En savoir plus</a>
+    <!-- BOUTON POUR LES DONS ET INFORMATIONS -->
+    <a class="bouton NS mt-3" onclick="openDonationPopup()">Faire un don</a><a class="bouton NS mt-3">En savoir plus</a>
+</div>
+
+
+<script>
+
+var currentWidth = (totalDons / 10000) * 100;
+
+function updateProgressBar() {
+var elem = document.getElementById("myBar");
+elem.style.width = currentWidth + "%";
+}
+
+updateProgressBar();
+
+function openDonationPopup() {
+<?php if (isset($_SESSION["nom"])) : ?>
+var donationAmount = prompt("Combien souhaitez-vous donner en € ?");
+if (donationAmount !== null) {
+    donate(donationAmount);
+}
+<?php else : ?>
+alert("Vous devez être connecté pour faire un don.");
+<?php endif; ?>
+}
+
+function donate(amount) {
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+if (xhr.readyState === XMLHttpRequest.DONE) {
+    alert("Don effectué avec succès !");
+    location.reload();
+}
+};
+xhr.open("POST", "../php/donation.php", true);
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhr.send("amount=" + amount);
+}
+
+</script>
+
+<div class="back">
+    <!-- DECTION DES DONS DE MATÉRIEL -->
+<h1 class="titres">Don Matériel</h1>
+<div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+<!-- Section en flexbox pour l'image et le texte -->
+<div class="mt-5 container flex1">
+    <span class="spanobj"><span class="titredon">Aidez-nous a Préserver Notre Culture</span><br><br>
+    Chaque geste contribue à tisser la toile de notre passé, créant un lien tangible entre hier, aujourd'hui et demain. Rejoins-nous dans cette aventure de préservation, car la restauration de notre patrimoine commence avec toi.<br></span>
+    <img class="imagedonOBJ" src="../images/donsOBJ.png"> 
     </div>
-    <!-- Javascript -->
-    <script src="../js/scriptdon.js"></script>
+    <!-- BOUTON POUR LES DONS ET INFORMATIONS -->
+    <div class="mt-5 d-flex justify-content-center mb-5">
+    <a class="bouton NS">Faire un don</a> <a class="bouton NS">En savoir plus</a>
+</div>
+<!-- Javascript -->
+<script src="../js/scriptdon.js"></script>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    
+
 </body>
 
-    <!-- Ajout du footer -->
-    <?php
-        include "../php/footer.php" ;
-    ?>
+<!-- Ajout du footer -->
+<?php
+    include "../php/footer.php" ;
+?>
 
 </html>
