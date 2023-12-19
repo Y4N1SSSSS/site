e2fc38cabadf77e1dc2e93c4528136c21d220ebd
hchugoch <hugo.cusant@gmail.com>
2023-12-19 17:04:38 +0100

    page planning
    création de la page planning

---
+++ pages/planning.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href='../css/stylepages.css' rel='stylesheet'>
+    <title>Planning</title>
+</head>
+    <!-- Ajout du header -->
+    <?php
+        include "../php/header.php" ;
+    ?>
+<body>
+    <!-- SECTION DES DONS EN EUROS -->
+    <h1 class="titres">Jour 1</h1>
+    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+    <!-- Section en flexbox pour l'image et le texte -->
+    <div class="container flexd mt-5 mb-3">
+        <img class="imagedon" src="../images/A COMPLETER"> 
+        - Rencontre entre les organisateurs et les volontaires <br>
+        - Création du plan d’action, quel ordre de restauration des patrimoines <br>
+        - Listings du matériel nécessaire pour les jours de restauration <br>
+        - Randonnée pédestre courte <br>
+        - Pique-nique conviviale (apporter à manger) <br>
+    </span>
+    </div>
+
+    <!-- SECTION DES DONS EN EUROS -->
+    <h1 class="titres">Jour 2</h1>
+    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+    <!-- Section en flexbox pour l'image et le texte -->
+    <div class="container flexd mt-5 mb-3">
+        - Rencontre entre les organisateurs et les volontaires <br>
+        - Création du plan d’action, quel ordre de restauration des patrimoines <br>
+        - Listings du matériel nécessaire pour les jours de restauration <br>
+        - Randonnée pédestre courte <br>
+        - Pique-nique conviviale (apporter à manger) <br>
+    </span>
+    <img class="imagedon" src="../images/A COMPLETER"> 
+    </div>
+
+    <!-- SECTION DES DONS EN EUROS -->
+    <h1 class="titres">Jour 3</h1>
+    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+    <!-- Section en flexbox pour l'image et le texte -->
+    <div class="container flexd mt-5 mb-3">
+        <img class="imagedon" src="../images/A COMPLETER"> 
+        - Rencontre entre les organisateurs et les volontaires <br>
+        - Création du plan d’action, quel ordre de restauration des patrimoines <br>
+        - Listings du matériel nécessaire pour les jours de restauration <br>
+        - Randonnée pédestre courte <br>
+        - Pique-nique conviviale (apporter à manger) <br>
+    </span>
+    </div>
+
+        <!-- SECTION DES DONS EN EUROS -->
+        <h1 class="titres">Jour 4</h1>
+    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+    <!-- Section en flexbox pour l'image et le texte -->
+    <div class="container flexd mt-5 mb-3">
+        - Rencontre entre les organisateurs et les volontaires <br>
+        - Création du plan d’action, quel ordre de restauration des patrimoines <br>
+        - Listings du matériel nécessaire pour les jours de restauration <br>
+        - Randonnée pédestre courte <br>
+        - Pique-nique conviviale (apporter à manger) <br>
+    </span>
+    <img class="imagedon" src="../images/A COMPLETER"> 
+    </div>
+
+        <!-- SECTION DES DONS EN EUROS -->
+        <h1 class="titres">Jour 5</h1>
+    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+    <!-- Section en flexbox pour l'image et le texte -->
+    <div class="container flexd mt-5 mb-3">
+        <img class="imagedon" src="../images/A COMPLETER"> 
+        - Rencontre entre les organisateurs et les volontaires <br>
+        - Création du plan d’action, quel ordre de restauration des patrimoines <br>
+        - Listings du matériel nécessaire pour les jours de restauration <br>
+        - Randonnée pédestre courte <br>
+        - Pique-nique conviviale (apporter à manger) <br>
+    </span>
+    </div>
+
+        <!-- SECTION DES DONS EN EUROS -->
+        <h1 class="titres">Jour 6</h1>
+    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+    <!-- Section en flexbox pour l'image et le texte -->
+    <div class="container flexd mt-5 mb-3">
+        - Rencontre entre les organisateurs et les volontaires <br>
+        - Création du plan d’action, quel ordre de restauration des patrimoines <br>
+        - Listings du matériel nécessaire pour les jours de restauration <br>
+        - Randonnée pédestre courte <br>
+        - Pique-nique conviviale (apporter à manger) <br>
+    </span>
+    <img class="imagedon" src="../images/A COMPLETER"> 
+    </div>
+
+        <!-- SECTION DES DONS EN EUROS -->
+        <h1 class="titres">Jour 7</h1>
+    <div class="ligne-container"> <div class="ligne-arrondie"></div> </div>
+    <!-- Section en flexbox pour l'image et le texte -->
+    <div class="container flexd mt-5 mb-3">
+        <img class="imagedon" src="../images/A COMPLETER"> 
+        - Rencontre entre les organisateurs et les volontaires <br>
+        - Création du plan d’action, quel ordre de restauration des patrimoines <br>
+        - Listings du matériel nécessaire pour les jours de restauration <br>
+        - Randonnée pédestre courte <br>
+        - Pique-nique conviviale (apporter à manger) <br>
+    </span>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    <!-- Javascript -->
+    <script src="../js/scriptdon.js"></script>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    
+</body>
+
+    <!-- Ajout du footer -->
+    <?php
+        include "../php/footer.php" ;
+    ?>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
