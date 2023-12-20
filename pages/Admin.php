2eb233f27bb3dc417be0d0cbeb98487b97ea88fa
Y4N1SSSSS <153291185+Y4N1SSSSS@users.noreply.github.com>
2023-12-20 20:28:24 +0100

    Création d'un système permettant l'accès à la page Admin
    Création d'un système permettant l'accès à la page Admin uniquement si l'on à l'identifiant admin pour notre compte utilisateur. Si l'on a pas cet identifiant on n'accède à une page miroir qui dit simplement que l'on ne peut pas accéder à la page

--- pages/Admin.php
+++ pages/Admin.php
@@ -1,59 +1,72 @@
-<?php require_once '../php/Config.php';
+<?php
+require_once '../php/Config.php';
 include '../php/header.php';
-  session_start(); 
-  try{
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-   }
-   catch(PDOException $e){
-      echo $e->getMessage();
-   }?>
-<section>
-    <h2> Espace Commentaire </h2>
-    <?php
-$requete='SELECT * FROM article ORDER BY ID_article';
-  $resultats=$pdo->query($requete);
-  $article=$resultats->fetchAll(PDO::FETCH_ASSOC);
-  $resultats->closeCursor();
-  ?>
-    <form action="../php/suppr.php" method="GET">
-        <select name="ID_article" required="required"> 
-        <?php
-            foreach($article as $commentaire):
-        ?>
-            <option value="<?php echo $commentaire["ID_article"];?>"> <?php echo $commentaire["Titre"];?> </option>
-        <?php
-            endforeach;
-        ?>  
-        </select><br/><br/>
-        <input type="submit" value="Supprimer"/>
-    </form>
+session_start();
 
-    <form action="../php/toutsuppr.php" method="GET">
-        <input type="submit" value="Tout Supprimer"/>
-    </form>
-</section>
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
 
-<section>
-<h2> Espace Liste participant </h2>
-<?php $query='SELECT * FROM participant ORDER BY ID_participant';
-  $find=$pdo->query($query);
-  $search=$find->fetchAll(PDO::FETCH_ASSOC);
-  $find->closeCursor(); ?>
-  <form action="../php/supprparticipant.php" method="GET">
-  <select name="ID_participant" required="required"> 
-        <?php
-            foreach($search as $participant):
-        ?>
-            <option value="<?php echo $participant["ID_participant"];?>"> <?php echo $participant["Nom_participant"];?> </option>
-        <?php
-            endforeach;
-        ?>  
-        </select><br/><br/>
-        <input type="submit" value="Supprimer"/>
-    </form>
-    <form action="../php/toutsupprparticipant.php" method="GET">
-        <input type="submit" value="Tout Supprimer"/>
-    </form>
+<?php if (isset($_SESSION["nom"])) : ?>
+    <?php if ($_SESSION['admin'] == 1) : ?>
+        <section>
+            <h2> Espace Commentaire </h2>
+            <?php
+            $requete = 'SELECT * FROM article ORDER BY ID_article';
+            $resultats = $pdo->query($requete);
+            $article = $resultats->fetchAll(PDO::FETCH_ASSOC);
+            $resultats->closeCursor();
+            ?>
+            <form action="../php/suppr.php" method="GET">
+                <select name="ID_article" required="required">
+                    <?php foreach ($article as $commentaire) : ?>
+                        <option value="<?= $commentaire["ID_article"] ?>"> <?= htmlspecialchars($commentaire["Titre"]) ?> </option>
+                    <?php endforeach; ?>
+                </select><br /><br />
+                <input type="submit" value="Supprimer" />
+            </form>
 
-</section>
+            <form action="../php/toutsuppr.php" method="GET">
+                <input type="submit" value="Tout Supprimer" />
+            </form>
+        </section>
+
+        <section>
+            <h2> Espace Liste participant </h2>
+            <?php
+            $query = 'SELECT * FROM participant ORDER BY ID_participant';
+            $find = $pdo->query($query);
+            $search = $find->fetchAll(PDO::FETCH_ASSOC);
+            $find->closeCursor();
+            ?>
+            <form action="../php/supprparticipant.php" method="GET">
+                <select name="ID_participant" required="required">
+                    <?php foreach ($search as $participant) : ?>
+                        <option value="<?= $participant["ID_participant"] ?>"> <?= htmlspecialchars($participant["Nom_participant"]) ?> </option>
+                    <?php endforeach; ?>
+                </select><br /><br />
+                <input type="submit" value="Supprimer" />
+            </form>
+            <form action="../php/toutsupprparticipant.php" method="GET">
+                <input type="submit" value="Tout Supprimer" />
+            </form>
+        </section>
+    <?php else : ?>
+        <h1 class="titres">Vous n'êtes pas autorisé à accéder à cette page</h1>
+        <div class="mt-5 d-flex justify-content-center">
+        <img class="w-25 mb-5" src="../images/non.gif"></img>
+        </div>
+    <?php endif; ?>
+<?php else : ?>
+    <h1 class="titres">Vous n'êtes pas autorisé à accéder à cette page</h1>
+        <div class="mt-5 d-flex justify-content-center">
+        <img class="w-25 mb-5 " src="../images/non.gif"></img>
+        </div>
+<?php endif; ?>
+
+<?php 
+include '../php/footer.php';
+?>
